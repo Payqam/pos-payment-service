eee495e9e777a27c9f41fbd0fcfc50c6f84954af
kokilagunawardhana <kokilack409@gmail.com>
2025-01-24 14:26:01 +0530

    fix(infrastructure): add stripe webhook

--- configurations/paths.ts
+++ configurations/paths.ts
@@ -1,6 +1,7 @@
 export const PATHS = {
-    FUNCTIONS:  {
-        TRANSACTIONS_PROCESS: '../src/functions/transaction-process',
-        SALESFORCE_SYNC: '../src/functions/salesforce-sync',
-    },
-} as const
+  FUNCTIONS: {
+    TRANSACTIONS_PROCESS: '../src/functions/transaction-process',
+    SALESFORCE_SYNC: '../src/functions/salesforce-sync',
+    STRIPE_WEBHOOK: '../src/functions/stripe-webhook',
+  },
+} as const;
