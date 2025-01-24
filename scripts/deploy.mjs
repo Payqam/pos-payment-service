9eced36cfc1a1fe97354b18a377523e7062203de
nadeesha-qriomatrix <107689664+nadeesha-qriomatrix@users.noreply.github.com>
2025-01-24 12:24:54 +0530

    fix(deploy): updated deploy script

--- scripts/deploy.mjs
+++ scripts/deploy.mjs
@@ -1,2 +1,2 @@
 #!/usr/bin/env zx
-await $`npx cdk@2.69.0 deploy -c env=${process.env.ENV} -c namespace=${process.env.NAMESPACE} --require-approval never --outputs-file ./cdk-outputs.json`;
+await $`npx cdk deploy -c env=${process.env.ENV} -c namespace=${process.env.NAMESPACE} --require-approval never --outputs-file ./cdk-outputs.json`;
