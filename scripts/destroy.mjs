d183450c51920d2e213653859dafc993970015ae
nadeesha-qriomatrix <107689664+nadeesha-qriomatrix@users.noreply.github.com>
2025-01-24 12:52:18 +0530

    feat(destroy): updated

--- scripts/destroy.mjs
+++ scripts/destroy.mjs
@@ -1,2 +1,2 @@
 #!/usr/bin/env zx
-// await $`npx cdk@2.69.0 destroy -c env=${process.env.ENV} -c namespace=${process.env.NAMESPACE} --force`;
+await $`npx cdk destroy -c env=${process.env.ENV} -c namespace=${process.env.NAMESPACE} --force`;
