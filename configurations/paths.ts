2f3826b3b3c3ff98f963070e5009d7dd2e28a7ff
kokilagunawardhana <kokilack409@gmail.com>
2025-01-27 09:51:42 +0530

    fix(deploy): fix eslint

--- configurations/paths.ts
+++ configurations/paths.ts
@@ -3,6 +3,6 @@ export const PATHS = {
     TRANSACTIONS_PROCESS: '../src/functions/transaction-process',
     SALESFORCE_SYNC: '../src/functions/salesforce-sync',
     STRIPE_WEBHOOK: '../src/functions/stripe-webhook',
-    WEBHOOK_ORANGE: '../src/functions/webhook/orange'
+    WEBHOOK_ORANGE: '../src/functions/webhook/orange',
   },
 } as const;
