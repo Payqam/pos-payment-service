d953d684d704ca225632a04f94ca52eb5105e8e4
Sudarshana Lakmal <sudarshana@qriomatrix.com>
2025-01-28 14:45:21 +0530

    feat(dynamodb): create construct for dynamodb table and indexes

---
+++ infrastructure/dynamodb.ts
@@ -0,0 +1,50 @@
+import { Construct } from "constructs";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as cdk from "aws-cdk-lib";
+
+export interface DynamoDBConstructProps {
+  envName: string;
+  namespace: string;
+  tableName: string;
+  removalPolicy?: cdk.RemovalPolicy;
+}
+
+export class DynamoDBConstruct extends Construct {
+  public readonly table: dynamodb.Table;
+
+  constructor(scope: Construct, id: string, props: DynamoDBConstructProps) {
+    super(scope, id);
+
+    // Create the main DynamoDB table
+    this.table = new dynamodb.Table(this, `${props.namespace}-Table`, {
+      tableName: `${props.namespace}-${props.envName}-${props.tableName}`,
+      partitionKey: {
+        name: "transactionId",
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: props.removalPolicy || cdk.RemovalPolicy.RETAIN,
+      pointInTimeRecovery: true,
+    });
+
+    // Add GSI1 with merchantId as partition key
+    this.table.addGlobalSecondaryIndex({
+      indexName: "GSI1",
+      partitionKey: {
+        name: "merchantId",
+        type: dynamodb.AttributeType.STRING,
+      },
+      projectionType: dynamodb.ProjectionType.ALL,
+    });
+  }
+
+  // Helper method to grant read/write permissions to other resources
+  public grantReadWrite(grantee: cdk.aws_iam.IGrantable): void {
+    this.table.grantReadWriteData(grantee);
+  }
+
+  // Helper method to grant read-only permissions to other resources
+  public grantRead(grantee: cdk.aws_iam.IGrantable): void {
+    this.table.grantReadData(grantee);
+  }
+}
