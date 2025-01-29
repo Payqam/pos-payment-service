623310e817211a697e46a006abd57c8568f1fe90
Sudarshana Lakmal <sudarshana@qriomatrix.com>
2025-01-29 18:24:42 +0530

    fix(build):revert changes

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -14,7 +14,18 @@ SECRETS.forEach((secret) => {
 // Read the environment variable values from the JSON file
 const envVarsPath = path.join(
   __dirname,
-  `../configurations/${env}.config.json`
+  `../configurations/workflow/${env}.config.json`
 );
+
+// Set up env variables
+const envVarsData = require(envVarsPath);
+for (const key in envVarsData) {
+  if (envVarsData.hasOwnProperty(key)) {
+    const value = envVarsData[key];
+    if (key && value) {
+      process.env[key.trim()] = value.trim();
+    }
+  }
+}
 
 await $`npm run build`;
