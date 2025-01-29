f3e154cb40c7ad86bade24d6fc72d17f2e0e1051
Sudarshana Lakmal <sudarshana@qriomatrix.com>
2025-01-29 18:19:14 +0530

    fix(build):remove unwanted environment variable injects

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -14,18 +14,7 @@ SECRETS.forEach((secret) => {
 // Read the environment variable values from the JSON file
 const envVarsPath = path.join(
   __dirname,
-  `../configurations/workflow/${env}.config.json`
+  `../configurations/${env}.config.json`
 );
-
-// Set up env variables
-const envVarsData = require(envVarsPath);
-for (const key in envVarsData) {
-  if (envVarsData.hasOwnProperty(key)) {
-    const value = envVarsData[key];
-    if (key && value) {
-      process.env[key.trim()] = value.trim();
-    }
-  }
-}
 
 await $`npm run build`;
