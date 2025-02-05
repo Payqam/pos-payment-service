c067b1c61aee4f189874cb4e5bc39dd5beea23ec
Yasiru Rasanjaya <yasiru.rasanjaya@qriomatrix.com>
2025-02-05 14:02:48 +0530

    feat(model): create payment model

---
+++ src/model/paymentRequest.ts
@@ -0,0 +1,15 @@
+interface CardData {
+  id: string;
+  cardName: string;
+  destinationId: string;
+}
+
+interface PaymentRequest {
+  amount: number;
+  paymentMethod: string;
+  cardData?: CardData;
+  customerPhone?: string;
+  metaData?: Record<string, string>;
+}
+
+export { CardData, PaymentRequest };
