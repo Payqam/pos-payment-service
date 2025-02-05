f909083cc8b3f3532071332fa5113af6f245ffbd
Yasiru Rasanjaya <yasiru.rasanjaya@qriomatrix.com>
2025-02-05 13:58:22 +0530

    feat(dynamodbClient): create dynamodb service constructor

---
+++ src/services/dynamodbService.ts
@@ -0,0 +1,102 @@
+import {
+  GetCommand,
+  GetCommandOutput,
+  NativeAttributeValue,
+  PutCommand,
+  UpdateCommand,
+} from '@aws-sdk/lib-dynamodb';
+import { DynamoDBDocClient } from '../dynamodbClient';
+import { CreatePaymentRecord } from '../model';
+import { buildUpdateExpression } from '../../utils/updateUtils';
+import { removeNullValues } from '../../utils/removeNullVavlues';
+import { ReturnValue } from '@aws-sdk/client-dynamodb';
+import { Logger, LoggerService } from '@mu-ts/logger';
+
+export class DynamoDBService {
+  private readonly logger: Logger;
+
+  private readonly tableName: string;
+
+  private dbClient: DynamoDBDocClient;
+
+  constructor() {
+    this.logger = LoggerService.named(this.constructor.name);
+    this.tableName = process.env.TRANSACTIONS_TABLE as string;
+    this.dbClient = DynamoDBDocClient.getInstance();
+    this.logger.info('init()');
+  }
+
+  /**
+   * Creates a payment record in the DynamoDB table.
+   *
+   * @param record - A plain object representing the payment record.
+   */
+  public async createPaymentRecord(record: CreatePaymentRecord): Promise<void> {
+    const params = {
+      TableName: this.tableName,
+      Item: record,
+    };
+
+    try {
+      await this.dbClient.sendCommand(new PutCommand(params));
+    } catch (error) {
+      this.logger.error('Error inserting record to DynamoDB', error);
+      throw error;
+    }
+  }
+
+  /**
+   * Updates a payment record in the DynamoDB table.
+   *
+   * @param key - The primary key of the record to update.
+   * @param updateFields - An object containing the fields to update.
+   *                     Any fields with null values will be removed.
+   */
+  public async updatePaymentRecord<T, U>(
+    key: T,
+    updateFields: U
+  ): Promise<void> {
+    const cleanedFields = removeNullValues({
+      ...updateFields,
+      updatedOn: Math.floor(Date.now() / 1000),
+    });
+    const expressionAttributeValues = {};
+    const { updateExpression, expressionAttributeNames } =
+      buildUpdateExpression(cleanedFields, expressionAttributeValues);
+    const params = {
+      TableName: this.tableName,
+      Key: key as Record<string, NativeAttributeValue>,
+      UpdateExpression: updateExpression,
+      ExpressionAttributeNames: expressionAttributeNames,
+      ExpressionAttributeValues: expressionAttributeValues,
+      ReturnValues: ReturnValue.ALL_NEW,
+    };
+
+    try {
+      await this.dbClient.updateCommandAsync(new UpdateCommand(params));
+    } catch (error) {
+      this.logger.error('Error updating record in DynamoDB', error);
+      throw error;
+    }
+  }
+
+  /**
+   * Retrieves an item from the DynamoDB table using GetItemCommand.
+   *
+   * @param key - The primary key of the record to retrieve.
+   * @returns The retrieved item wrapped in a GetItemCommandOutput.
+   */
+  public async getItem<T>(key: T): Promise<GetCommandOutput> {
+    const params = {
+      TableName: this.tableName,
+      Key: key as Record<string, NativeAttributeValue>,
+    };
+    try {
+      const result = await this.dbClient.getItem(new GetCommand(params));
+      return result as GetCommandOutput;
+    } catch (error) {
+      this.logger.error('Error retrieving record from DynamoDB', error);
+      throw error;
+    }
+  }
+}
