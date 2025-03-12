b11ab51d1b97c8e00bcb64cae2b448d2fe5704f4
Sudarshana Lakmal <sudarshana@qriomatrix.com>
2025-03-12 17:32:15 +0530

    feat(vpc):configure VPC as a shared resource

--- infrastructure/vpc.ts
+++ infrastructure/vpc.ts
@@ -1,5 +1,6 @@
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib';
 
 /**
  * PaymentServiceVPC creates a minimal VPC configuration:
@@ -8,37 +9,19 @@ import { Construct } from 'constructs';
  * - Total IP addresses: 128 (optimized for small deployments)
  * - Maintains high availability across 2 AZs
  */
+interface PaymentServiceVPCProps extends cdk.StackProps {
+  appVpcId: string;
+}
+
 export class PaymentServiceVPC extends Construct {
   public readonly vpc: ec2.IVpc;
 
-  constructor(scope: Construct, id: string) {
+  constructor(scope: Construct, id: string, props: PaymentServiceVPCProps) {
     super(scope, id);
 
-    // Try to get an existing VPC by name
-    this.vpc = ec2.Vpc.fromLookup(this, 'ExistingVPC', {
-      isDefault: false, // Ensure it's not the default VPC
-      vpcName: 'PaymentServiceSharedVPC',
+    // Try to get App VPC
+    this.vpc = ec2.Vpc.fromLookup(this, 'PayQam-App-VPC', {
+      vpcId: props.appVpcId,
     });
-
-    if (!this.vpc) {
-      this.vpc = new ec2.Vpc(this, 'PaymentServiceVPC', {
-        vpcName: 'PaymentServiceSharedVPC',
-        maxAzs: 2,
-        ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/24'), // Smaller CIDR range
-        subnetConfiguration: [
-          {
-            cidrMask: 27, // 32 IPs per subnet
-            name: 'Public',
-            subnetType: ec2.SubnetType.PUBLIC,
-          },
-          {
-            cidrMask: 27, // 32 IPs per subnet
-            name: 'Private',
-            subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
-          },
-        ],
-        natGateways: 1, // Reduce to 1 NAT Gateway to save costs
-      });
-    }
   }
 }
