4e9258425097c12924024374e4a27e3ccd854020
SehaniChathurangi <sehani.chathurangi@qriomatrix.com>
2025-03-14 15:00:10 +0530

    feat(testing):add provider test cases and refactor scripts

---
+++ test/mock/mtn_mock.cy.ts
@@ -0,0 +1,271 @@
+import testData from '../../cypress/fixtures/mtn_test_data.json';
+let transactionId, uniqueId, accessToken;
+
+describe('MTN Payment Processing Tests', () => {
+  testData.requestPayer.forEach((test) => {
+    describe(`Validate ${test.title} Payment Processing`, () => {
+      it('should process a payment', () => {
+        cy.request({
+          method: 'POST',
+          url: `${Cypress.env('MTNServiceEndpoint')}/transaction/process/charge`,
+          headers: {
+            'x-api-key': `${Cypress.env('MTNApiKey')}`,
+            'Content-Type': 'application/json',
+          },
+          body: {
+            merchantId: 'MERCHANT_123',
+            merchantMobileNo: test.merchant,
+            amount: 100,
+            customerPhone: test.payer,
+            transactionType: 'CHARGE',
+            paymentMethod: 'MTN',
+            currency: 'EUR',
+            metaData: {
+              reference: 'ORDER_123',
+              description: 'Payment for order #123',
+            },
+          },
+        }).then((response) => {
+          expect(response.status).to.eq(200);
+          cy.task('log', response.body);
+          expect(response.body).to.have.property(
+            'message',
+            'Payment processed successfully'
+          );
+          expect(response.body).to.have.property('transactionDetails');
+          expect(response.body.transactionDetails).to.have.property(
+            'transactionId'
+          );
+          expect('PAYMENT_REQUEST_CREATED').to.include(
+            response.body.transactionDetails.status
+          );
+
+          transactionId = response.body.transactionDetails.transactionId;
+          cy.task('log', `Transaction ID : ${transactionId}`);
+          Cypress.env('transactionId', transactionId);
+          cy.wait(500);
+        });
+      });
+
+      it('should get transaction status', () => {
+        cy.request({
+          method: 'GET',
+          url: `${Cypress.env('MTNServiceEndpoint')}/transaction/status/?transactionId=${Cypress.env('transactionId')}`,
+          headers: {
+            'x-api-key': `${Cypress.env('MTNApiKey')}`,
+            'Content-Type': 'application/json',
+          },
+        }).then((response) => {
+          expect(response.status).to.eq(200);
+          cy.task('log', response.body);
+          expect(response.body).to.have.property(
+            'message',
+            'Transaction retrieved successfully'
+          );
+          expect(response.body.transaction.Item).to.have.property(
+            'status',
+            'PAYMENT_REQUEST_CREATED'
+          );
+          uniqueId = response.body.transaction.Item.uniqueId;
+          cy.task('log', ` ${uniqueId}`);
+          Cypress.env('uniqueId', uniqueId);
+          cy.task('log', response.body);
+        });
+      });
+
+      it('Generates an Access Token', () => {
+        cy.request({
+          method: 'POST',
+          url: 'https://sandbox.momodeveloper.mtn.com/collection/token/',
+          headers: {
+            'Ocp-Apim-Subscription-Key': `${Cypress.env('MTNCollectionSubscriptionKey')}`,
+            Authorization:
+              'Basic ' +
+              btoa(
+                `${Cypress.env('MTNCollectionApiUser')}:${Cypress.env('MTNCollectionApiKey')}`
+              ),
+          },
+        }).then((response) => {
+          expect(response.status).to.eq(200);
+          accessToken = response.body.access_token;
+          cy.task('log', accessToken);
+          Cypress.env('accessToken', accessToken);
+        });
+      });
+
+      it('Checks Transaction Status', () => {
+        cy.request({
+          method: 'GET',
+          url: `https://sandbox.momodeveloper.mtn.com/collection/v1_0/requesttopay/${Cypress.env('transactionId')}`,
+          headers: {
+            'X-Target-Environment': 'sandbox',
+            Authorization: `Bearer ${Cypress.env('accessToken')}`,
+            'Ocp-Apim-Subscription-Key': `${Cypress.env('MTNCollectionSubscriptionKey')}`,
+          },
+        }).then((response) => {
+          expect(response.status).to.eq(200);
+          cy.task('log', response.body);
+          expect(response.body).to.have.property('status', 'PENDING');
+        });
+      });
+    });
+  });
+});
+
+testData.refundTestData.forEach((test) => {
+  describe('Validate Refund process', () => {
+    describe('Payment Process', () => {
+      it('should process a payment', () => {
+        cy.request({
+          method: 'POST',
+          url: `${Cypress.env('MTNServiceEndpoint')}/transaction/process/charge`,
+          headers: {
+            'x-api-key': `${Cypress.env('MTNApiKey')}`,
+            'Content-Type': 'application/json',
+          },
+          body: {
+            merchantId: 'MERCHANT_123',
+            merchantMobileNo: test.merchant,
+            amount: 200,
+            customerPhone: test.payer,
+            transactionType: 'CHARGE',
+            paymentMethod: 'MTN',
+            currency: 'EUR',
+            metaData: {
+              reference: 'ORDER_123',
+              description: 'Payment for order #123',
+            },
+          },
+        }).then((response) => {
+          expect(response.status).to.eq(200);
+          cy.task('log', response.body);
+          expect(response.body).to.have.property(
+            'message',
+            'Payment processed successfully'
+          );
+          expect(response.body).to.have.property('transactionDetails');
+          expect(response.body.transactionDetails).to.have.property(
+            'transactionId'
+          );
+          expect('PAYMENT_REQUEST_CREATED').to.include(
+            response.body.transactionDetails.status
+          );
+
+          transactionId = response.body.transactionDetails.transactionId;
+          cy.task('log', `Transaction ID : ${transactionId}`);
+          Cypress.env('transactionId', transactionId);
+          cy.wait(500);
+        });
+      });
+
+      it('should get transaction status', () => {
+        cy.request({
+          method: 'GET',
+          url: `${Cypress.env('MTNServiceEndpoint')}/transaction/status/?transactionId=${Cypress.env('transactionId')}`,
+          headers: {
+            'x-api-key': `${Cypress.env('MTNApiKey')}`,
+            'Content-Type': 'application/json',
+          },
+        }).then((response) => {
+          expect(response.status).to.eq(200);
+          cy.task('log', response.body);
+          expect(response.body).to.have.property(
+            'message',
+            'Transaction retrieved successfully'
+          );
+          expect(response.body.transaction.Item).to.have.property(
+            'status',
+            'DISBURSEMENT_SUCCESSFUL'
+          );
+          uniqueId = response.body.transaction.Item.uniqueId;
+          cy.task('log', ` ${uniqueId}`);
+          Cypress.env('uniqueId', uniqueId);
+        });
+      });
+    });
+
+    describe('Refund Process', () => {
+      it('Process Payment Refund', () => {
+        cy.request({
+          method: 'POST',
+          url: `${Cypress.env('MTNServiceEndpoint')}/transaction/process/refund`,
+          headers: {
+            'x-api-key': `${Cypress.env('MTNApiKey')}`,
+            'Content-Type': 'application/json',
+          },
+          body: {
+            transactionType: 'REFUND',
+            paymentMethod: 'MTN',
+            transactionId: `${Cypress.env('transactionId')}`,
+          },
+        }).then((response) => {
+          expect(response.status).to.eq(200);
+          cy.task('log', response.body);
+          expect(response.body).to.have.property(
+            'message',
+            'Payment processed successfully'
+          );
+          expect(response.body).to.have.property('transactionDetails');
+          expect(response.body.transactionDetails).to.have.property(
+            'status',
+            'CUSTOMER_REFUND_REQUEST_CREATED'
+          );
+        });
+      });
+
+      it('Verify Original Transaction Refund Status', () => {
+        cy.request({
+          method: 'GET',
+          url: `${Cypress.env('MTNServiceEndpoint')}/transaction/status/?transactionId=${Cypress.env('transactionId')}`,
+          headers: {
+            'x-api-key': `${Cypress.env('MTNApiKey')}`,
+          },
+        }).then((response) => {
+          expect(
+            response.body.transaction.Item.customerRefundResponse
+          ).to.have.property('amount', '200');
+          expect(response.body.transaction.Item.status).to.eq(
+            'MERCHANT_REFUND_SUCCESSFUL'
+          );
+          cy.task('log', response.body);
+        });
+      });
+
+      it('Generates an Access Token', () => {
+        cy.request({
+          method: 'POST',
+          url: 'https://sandbox.momodeveloper.mtn.com/collection/token/',
+          headers: {
+            'Ocp-Apim-Subscription-Key': `${Cypress.env('MTNCollectionSubscriptionKey')}`,
+            Authorization:
+              'Basic ' +
+              btoa(
+                `${Cypress.env('MTNCollectionApiUser')}:${Cypress.env('MTNCollectionApiKey')}`
+              ),
+          },
+        }).then((response) => {
+          expect(response.status).to.eq(200);
+          accessToken = response.body.access_token;
+          cy.task('log', accessToken);
+          Cypress.env('accessToken', accessToken);
+        });
+      });
+
+      it('Checks Transaction Status from merchant to PayQam', () => {
+        cy.request({
+          method: 'GET',
+          url: `https://sandbox.momodeveloper.mtn.com/collection/v1_0/requesttopay/${Cypress.env('transactionId')}`,
+          headers: {
+            'X-Target-Environment': 'sandbox',
+            Authorization: `Bearer ${Cypress.env('accessToken')}`,
+            'Ocp-Apim-Subscription-Key': `${Cypress.env('MTNCollectionSubscriptionKey')}`,
+          },
+        }).then((response) => {
+          expect(response.status).to.eq(200);
+          cy.task('log', response.body);
+          expect(response.body).to.have.property('status', 'SUCCESSFUL');
+        });
+      });
+    });
+  });
+});
