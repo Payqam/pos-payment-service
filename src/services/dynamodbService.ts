6291501c84a14c121bcb6864a652e9a5b0503433
Yasiru Rasanjaya <yasiru.rasanjaya@qriomatrix.com>
2025-03-25 14:24:59 +0530

    feat(dynamodb): update dynamodb service

--- src/services/dynamodbService.ts
+++ src/services/dynamodbService.ts
@@ -162,6 +162,7 @@ export class DynamoDBService {
       ExpressionAttributeNames: expressionAttributeNames,
       ExpressionAttributeValues: expressionAttributeValues,
       ReturnValues: ReturnValue.ALL_NEW,
+      removeNullValues: true,
     };
 
     let attempt = 0;
